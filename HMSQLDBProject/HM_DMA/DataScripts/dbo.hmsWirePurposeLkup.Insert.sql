a91a3430a0737d00bdae9d098860b8743f136324
Venkata Mane <VenkataSandeep.Mane@bnymellon.com>
2019-05-15 17:05:50 +0530

    ASTDMP-7:AC-2724: Wire Initiation Changes. Committing the changes for Wire Initiation in HM Ops.

--- HMSQLDBProject/HM_DMA/DataScripts/dbo.hmsWirePurposeLkup.Insert.sql
+++ HMSQLDBProject/HM_DMA/DataScripts/dbo.hmsWirePurposeLkup.Insert.sql
@@ -3,3 +3,12 @@ BEGIN
 INSERT INTO hmsWirePurposeLkup (ReportName,Purpose) VALUES ('Collateral','Send Call')
 END
 GO
+
+IF NOT EXISTS(SELECT * FROM hmsWireTransferTypeLKup WHERE TransferType = 'Notice')
+BEGIN
+INSERT INTO hmsWireTransferTypeLKup ([TransferType]) VALUES ('Notice');
+END
+GO
+
+
+
--- HMSQLDBProject/HM_DMA/DataScripts/dbo.hmsWires.UpdateScript.sql
+++ HMSQLDBProject/HM_DMA/DataScripts/dbo.hmsWires.UpdateScript.sql
@@ -3,6 +3,3 @@ BEGIN
 UPDATE hmsWires SET WireTransferTypeId = 3 WHERE WireTransferTypeId > 3
 END
 GO
-
-DELETE FROM hmsWireTransferTypeLKup WHERE WireTransferTypeId > 3
-GO
