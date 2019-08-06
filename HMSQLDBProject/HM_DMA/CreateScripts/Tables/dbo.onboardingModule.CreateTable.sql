78a44d7f93645c0e785b0ce1e6dcf777d3b5a087
Venkata Mane <VenkataSandeep.Mane@bnymellon.com>
2019-08-06 13:49:05 +0530

    ASTDMP-7: Renaming the SQL Script.

--- HMSQLDBProject/HM_DMA/CreateScripts/Tables/dbo.onboardingModule.CreateTable.sql
+++ HMSQLDBProject/HM_DMA/CreateScripts/Tables/dbo.onboardingModule.CreateTable.sql
@@ -1,35 +1,3 @@
-IF NOT EXISTS(SELECT * FROM sys.objects WHERE type='U' AND name='onboardingAccountModule')
-BEGIN
-	CREATE TABLE [dbo].[onboardingAccountModule]
-	(
-		[onboardingAccountModuleId] BIGINT NOT NULL IDENTITY(1,1),
-		[Module] VARCHAR(100) NOT NULL,
-		[CreatedAt] DATETIME NOT NULL DEFAULT(GETDATE()),
-		[CreatedBy] VARCHAR(100) NOT NULL,
-		CONSTRAINT onboardingAccountModule_PK PRIMARY KEY NONCLUSTERED (onboardingAccountModuleId)
-	)
-END
-GO 
-
-IF NOT EXISTS(SELECT 8 FROM onboardingAccountModule)
-BEGIN
-
-INSERT INTO [onboardingAccountModule] ([Module],[CreatedAt],[CreatedBy]) VALUES('Collateral', GETDATE(), 'Retrofit')
-INSERT INTO [onboardingAccountModule] ([Module],[CreatedAt],[CreatedBy]) VALUES('Invoice', GETDATE(), 'Retrofit')
-INSERT INTO [onboardingAccountModule] ([Module],[CreatedAt],[CreatedBy]) VALUES('Expenses', GETDATE(), 'Retrofit')
-INSERT INTO [onboardingAccountModule] ([Module],[CreatedAt],[CreatedBy]) VALUES('Margin', GETDATE(), 'Retrofit')
-INSERT INTO [onboardingAccountModule] ([Module],[CreatedAt],[CreatedBy]) VALUES('Cash', GETDATE(), 'Retrofit')
-INSERT INTO [onboardingAccountModule] ([Module],[CreatedAt],[CreatedBy]) VALUES('Repo Collateral', GETDATE(), 'Retrofit')
-INSERT INTO [onboardingAccountModule] ([Module],[CreatedAt],[CreatedBy]) VALUES('Interest Rate', GETDATE(), 'Retrofit')
-
-END
-GO
-
-IF EXISTS(SELECT * FROM sys.objects WHERE TYPE = 'U' AND NAME ='onboardingAccountModule') AND EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE COLUMN_NAME = 'Module' AND TABLE_NAME ='onboardingAccountModule')
-BEGIN
-DROP TABLE onboardingAccountModule
-END
-
 IF NOT EXISTS(SELECT * FROM sys.objects WHERE type='U' AND name='onBoardingModule')
 BEGIN
 	CREATE TABLE [dbo].[onBoardingModule]
