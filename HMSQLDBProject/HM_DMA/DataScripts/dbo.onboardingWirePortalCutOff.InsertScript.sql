70a2dcb7390ace989e8d67171e333a963446dad6
Venkata Mane <VenkataSandeep.Mane@bnymellon.com>
2019-11-19 15:50:30 +0530

    HMOS-12: Add Wire cut-off lines in the table with cash mechanism

---
+++ HMSQLDBProject/HM_DMA/DataScripts/dbo.onboardingWirePortalCutOff.InsertScript.sql
@@ -0,0 +1,48 @@
+
+IF NOT EXISTS (SELECT 8 FROM onBoardingWirePortalCutoff WHERE CashInstruction = 'BNP Neolink' AND Country = 'United Kingdom' AND Currency = 'GBP')
+BEGIN
+INSERT INTO onBoardingWirePortalCutoff (CashInstruction, Country, Currency, CutOffTime, DaystoWire) VALUES ('BNP Neolink', 'United Kingdom', 'GBP', '02:10:00', 0)
+END
+GO
+
+IF NOT EXISTS (SELECT 8 FROM onBoardingWirePortalCutoff WHERE CashInstruction = 'BNP Neolink' AND Country = 'EU' AND Currency = 'EUR')
+BEGIN
+INSERT INTO onBoardingWirePortalCutoff (CashInstruction, Country, Currency, CutOffTime, DaystoWire) VALUES ('BNP Neolink', 'EU', 'EUR', '10:20:00', 0)
+END
+GO
+
+IF NOT EXISTS (SELECT 8 FROM onBoardingWirePortalCutoff WHERE CashInstruction = 'BNP Neolink' AND Country = 'Japan' AND Currency = 'JPY')
+BEGIN
+INSERT INTO onBoardingWirePortalCutoff (CashInstruction, Country, Currency, CutOffTime, DaystoWire) VALUES ('BNP Neolink', 'Japan', 'JPY', '11:45:00', 1)
+END
+GO
+
+IF NOT EXISTS (SELECT 8 FROM onBoardingWirePortalCutoff WHERE CashInstruction = 'BNP Neolink' AND Country = 'USA' AND Currency = 'USD')
+BEGIN
+INSERT INTO onBoardingWirePortalCutoff (CashInstruction, Country, Currency, CutOffTime, DaystoWire) VALUES ('BNP Neolink', 'USA', 'USD', '04:00:00', 0)
+END
+GO
+
+IF NOT EXISTS (SELECT 8 FROM onBoardingWirePortalCutoff WHERE CashInstruction = 'BBH WorldView' AND Country = 'United Kingdom' AND Currency = 'GBP')
+BEGIN
+INSERT INTO onBoardingWirePortalCutoff (CashInstruction, Country, Currency, CutOffTime, DaystoWire) VALUES ('BBH WorldView', 'United Kingdom', 'GBP', '10:00:00', 0)
+END
+GO
+
+IF NOT EXISTS (SELECT 8 FROM onBoardingWirePortalCutoff WHERE CashInstruction = 'BBH WorldView' AND Country = 'EU' AND Currency = 'EUR')
+BEGIN
+INSERT INTO onBoardingWirePortalCutoff (CashInstruction, Country, Currency, CutOffTime, DaystoWire) VALUES ('BBH WorldView', 'EU', 'EUR', '10:00:00', 0)
+END
+GO
+
+IF NOT EXISTS (SELECT 8 FROM onBoardingWirePortalCutoff WHERE CashInstruction = 'BBH WorldView' AND Country = 'Japan' AND Currency = 'JPY')
+BEGIN
+INSERT INTO onBoardingWirePortalCutoff (CashInstruction, Country, Currency, CutOffTime, DaystoWire) VALUES ('BBH WorldView', 'Japan', 'JPY', '22:00:00', 0)
+END
+GO
+
+IF NOT EXISTS (SELECT 8 FROM onBoardingWirePortalCutoff WHERE CashInstruction = 'BBH WorldView' AND Country = 'USA' AND Currency = 'USD')
+BEGIN
+INSERT INTO onBoardingWirePortalCutoff (CashInstruction, Country, Currency, CutOffTime, DaystoWire) VALUES ('BBH WorldView', 'USA', 'USD', '17:30:00', 0)
+END
+GO
