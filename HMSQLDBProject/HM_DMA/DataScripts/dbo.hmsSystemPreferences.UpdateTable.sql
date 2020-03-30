a8e12fd14214b67d66a46667cbc38de802785afa
nadhiya rajendran <nadhiya.rajendran@bnymellon.com>
2020-03-30 13:14:33 +0530

    HMDMA-964:Branching HM_ops v34.0 and HM_ops_Secure v5.0 Branching v5.0

--- HMSQLDBProject/HM_DMA/DataScripts/dbo.hmsSystemPreferences.UpdateTable.sql
+++
@@ -1,5 +0,0 @@
-IF NOT EXISTS(SELECT 8 FROM hmsSystemPreferences WHERE SystemKey = 'ReceivingAgreementTypesForAccount')
-BEGIN
- INSERT INTO hmsSystemPreferences (SystemKey, SystemValue) VALUES('ReceivingAgreementTypesForAccount', 'FCM,CDA,ISDA,GMRA,MRA,MSFTA,FXPB')
-END
-GO
