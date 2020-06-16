40d58e927c1249a089a9864f6663703a7a4b2411
Silvester Tony <SilvesterTony.MariyaSingam@bnymellon.com>
2020-06-16 13:17:38 +0530

    HMOS-71 Ability for SSI Template to have Multiple FFC details OnboardingModule table Treasury entry is removed.

--- HMSQLDBProject/HM_DMA/DataScripts/dbo.onBoardingModule.UpdateTable.sql
+++ HMSQLDBProject/HM_DMA/DataScripts/dbo.onBoardingModule.UpdateTable.sql
@@ -1,7 +1,7 @@
 
 USE HM_WIRES
-IF NOT EXISTS(SELECT * FROM onBoardingModule WHERE ModuleName = 'Treasury')
+IF EXISTS(SELECT * FROM onBoardingModule WHERE ModuleName = 'Treasury')
 BEGIN
-INSERT INTO onBoardingModule VALUES (3, 'Treasury', GETDATE(), 'System')
+delete from onBoardingModule where ModuleName = 'Treasury'
 END
 GO
