97e4ad2ed4e5300c189b832efc66846d140ff47f
Mohan Srikanth Rameshkumar <mohansrikant.rameshkumar@bnymellon.com>
2020-07-23 08:52:37 -0400

    OB-97:Fund Summary View Committing code to add more required fields to fund accounts view

--- HMSQLDBProject/HM_DMA/CreateScripts/Views/dbo.vw_FundAccounts.CreateViewsql.sql
+++ HMSQLDBProject/HM_DMA/CreateScripts/Views/dbo.vw_FundAccounts.CreateViewsql.sql
@@ -18,7 +18,8 @@ BEGIN
 				acc.dmaCounterpartyId, cp.CounterpartyName,cpf.CounterpartyFamily,acc.AccountName, 
 				CASE WHEN LEN( acc.FFCNumber) >0 THEN acc.FFCNumber
 				ELSE acc.UltimateBeneficiaryAccountNumber  END AS AccountNumber,
-				acc.FFCNumber,acc.UltimateBeneficiaryAccountNumber,acc.Currency,acc.AccountPurpose,acc.AccountStatus
+				acc.FFCNumber,acc.UltimateBeneficiaryAccountNumber,acc.Currency,acc.AccountPurpose,acc.AccountStatus,
+				acc.[Description],acc.TickerorISIN,acc.CashSweep,acc.CashSweepTime,acc.CashSweepTimeZone
 				FROM onBoardingAccount acc
 				LEFT JOIN HM.DBO.ClientFund CF WITH(NOLOCK)   ON CF.ClientFundID=acc.hmFundId
 				LEFT JOIN HM.DBO.Fund F  WITH(NOLOCK) ON F.FundID = CF.FundID  
