a4be79680f70c74ae4c3aca3816d48561e9978f5
nadhiya rajendran <nadhiya.rajendran@bnymellon.com>
2020-09-09 14:23:57 +0530

    HMDMA-1380:Accounts label change for repo colateral

--- HMSQLDBProject/HM_DMA/DataScripts/dbo.onboardingModule.InsertScript.sql
+++ HMSQLDBProject/HM_DMA/DataScripts/dbo.onboardingModule.InsertScript.sql
@@ -4,7 +4,7 @@ BEGIN
 
 INSERT INTO [onBoardingModule] ([dmaReportsId],[ModuleName],[CreatedAt],[CreatedBy]) VALUES(17, 'Repo Collateral Pledge', GETDATE(), 'system')
 INSERT INTO [onBoardingModule] ([dmaReportsId],[ModuleName],[CreatedAt],[CreatedBy]) VALUES(17, 'Repo Collateral Return', GETDATE(), 'system')
-INSERT INTO [onBoardingModule] ([dmaReportsId],[ModuleName],[CreatedAt],[CreatedBy]) VALUES(17, 'Repo Collateral Payment', GETDATE(), 'system')
+
 END
 GO 
 
