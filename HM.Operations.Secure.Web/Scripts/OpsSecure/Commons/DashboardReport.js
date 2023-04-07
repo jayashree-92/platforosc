87af59777a7e9d24f9c5abfd9680dcd98e0d99ca
Mohansrikant RameshKumar <Mohansrikant.RameshKumar@innocap.com>
2023-04-07 22:40:47 +0530

    HMDMA-3256:Scheduling should be allowed only when all existing preference changes are saved

--- HM.Operations.Secure.Web/Scripts/OpsSecure/Commons/DashboardReport.js
+++ HM.Operations.Secure.Web/Scripts/OpsSecure/Commons/DashboardReport.js
@@ -141,7 +141,7 @@ HmOpsApp.controller("dashboardReportCtrl", function ($scope, $http, $interval, $
                     formatResult: formatSelect,
                     formatSelection: formatSelect
                 });
-
+                $("#li" + v.Preference).select2("val", "");
             });
         });
     }
