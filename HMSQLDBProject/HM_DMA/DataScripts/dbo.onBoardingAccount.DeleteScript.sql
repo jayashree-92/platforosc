c1d87c2f717b977c2c5e9830d488731dbe912b27
Nadhiya Rajendran <nadhiya.rajendran@innocap.com>
2023-05-08 18:48:32 +0530

    HMOS-404:Create a script to Delete all Accounts which are DDA/Custody and are in Saved as Draft status

--- HMSQLDBProject/HM_DMA/DataScripts/dbo.onBoardingAccount.DeleteScript.sql
+++ HMSQLDBProject/HM_DMA/DataScripts/dbo.onBoardingAccount.DeleteScript.sql
@@ -1,9 +1,9 @@
 USE HM_WIRES
 GO
 
-IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'onBoardingAccount_bkup_V21_0' AND TABLE_SCHEMA = 'DmaBackup')
+IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'onBoardingAccount_bkup_V21_1' AND TABLE_SCHEMA = 'DmaBackup')
 BEGIN
-	SELECT * INTO DmaBackup.onBoardingAccount_bkup_V21_0 FROM onBoardingAccount
+	SELECT * INTO DmaBackup.onBoardingAccount_bkup_V21_1 FROM onBoardingAccount
 	UPDATE onBoardingAccount SET IsDeleted=1 WHERE AccountType in ('Custody','DDA') and onboardingAccountStatus in ('Created') and isdeleted=0 and updatedat<'2023-01-01'
 END
 GO
