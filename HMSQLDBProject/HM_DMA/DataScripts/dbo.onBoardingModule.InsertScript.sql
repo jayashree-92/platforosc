0fd4eb780f49c091506f10201dfc4900ea91e09a
Silvester Tony <silvestertony.mariyasingam@innocap.com>
2023-06-28 13:18:19 +0530

    HMOS-424 Create a new module called - OTC Settlements in secure site script is added.

---
+++ HMSQLDBProject/HM_DMA/DataScripts/dbo.onBoardingModule.InsertScript.sql
@@ -0,0 +1,14 @@
+use HM_WIRES
+
+IF NOT EXISTS( SELECT * FROM onBoardingModule where dmaReportsId = 22 and ModuleName = 'OTC Settlements')
+BEGIN
+INSERT INTO onBoardingModule VALUES (22, 'OTC Settlements', GETDATE(), 'SYSTEM')
+END
+GO
+
+
+IF NOT EXISTS(SELECT * FROM hmsWirePurposeLkup where ReportName = 'OTC Trade Lifecycle')
+BEGIN
+INSERT INTO hmsWirePurposeLkup VALUES ('OTC Trade Lifecycle', 'OTC Settlements', -1, GETDATE(), NULL,NULL,1)
+END
+GO
