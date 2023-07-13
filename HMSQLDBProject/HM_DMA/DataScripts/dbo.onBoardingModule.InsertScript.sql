5d1cd799cf68c011a896423bcb2008d42b6e880a
Mohan Srikanth <mohansrikant.rameshkumar@innocap.com>
2023-07-13 18:03:34 +0100

    HMOS-422:Wire attachment not downloading for .msg formats

--- HMSQLDBProject/HM_DMA/DataScripts/dbo.onBoardingModule.InsertScript.sql
+++ HMSQLDBProject/HM_DMA/DataScripts/dbo.onBoardingModule.InsertScript.sql
@@ -6,9 +6,14 @@ INSERT INTO onBoardingModule VALUES (22, 'OTC Settlements', GETDATE(), 'SYSTEM')
 END
 GO
 
-
 IF NOT EXISTS(SELECT * FROM hmsWirePurposeLkup where ReportName = 'OTC Trade Lifecycle')
 BEGIN
 INSERT INTO hmsWirePurposeLkup VALUES ('OTC Trade Lifecycle', 'OTC Settlements', -1, GETDATE(), NULL,NULL,1)
+END
+GO
+
+IF NOT EXISTS(SELECT * FROM hmsWirePurposeLkup where ReportName = 'Treasury')
+BEGIN
+INSERT INTO hmsWirePurposeLkup VALUES ('Treasury', 'Cash Optimization', -1, GETDATE(), NULL,NULL,1)
 END
 GO
