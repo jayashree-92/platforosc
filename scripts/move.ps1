bc8a486df7c54a27631dedac843a455ecc1fdd22
Hamza Errkiba <hamza.errkiba@innocap.com>
2024-08-22 13:59:01 +0000

    Updated move.ps1

--- scripts/move.ps1
+++ scripts/move.ps1
@@ -1,3 +1,8 @@
+Expand-Archive -Path "C:\azure-devops-deployment\HM.Operations.Secure.Web.zip" -DestinationPath "C:\azure-devops-deployment -Force
+Copy-Item -Path "C:\azure-devops-deployment\Content\C_C\agent-ic-admin-dev\_work\2\s\HM.Operations.Secure.Web\obj\Local\Package\PackageTmp" -Destination "C:\azure-devops-deployment\deployment" -Recurse -Force
+
+
+
 # Variables
 $sourcePath = "C:\azure-devops-deployment\deployment"   # Replace with the path to your source repo
 $destinationPath = "\\vmwclnwosdusc01\devops-share" # Replace with the path to your destination repo
