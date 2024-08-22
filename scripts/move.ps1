770ef62f64996bdfa6afe486cad0a448d6d110ba
Hamza Errkiba <hamza.errkiba@innocap.com>
2024-08-22 14:33:48 +0000

    Updated move.ps1

--- scripts/move.ps1
+++ scripts/move.ps1
@@ -1,3 +1,3 @@
 Expand-Archive -Path "C:\azure-devops-deployment\HM.Operations.Secure.Web.zip" -DestinationPath "C:\azure-devops-deployment -Force
 Copy-Item -Path "C:\azure-devops-deployment\Content\C_C\agent-ic-admin-dev\_work\2\s\HM.Operations.Secure.Web\obj\Local\Package\PackageTmp" -Destination "C:\azure-devops-deployment" -Recurse -Force
- Copy-Item -Path "C:\azure-devops-deployment\PackageTmp" -Destination \\vmwclnwosdusc01\devops-share -Recurse -Force
+ Copy-Item -Path "C:\azure-devops-deployment\PackageTmp\*" -Destination \\vmwclnwosdusc01\devops-share -Recurse -Force
